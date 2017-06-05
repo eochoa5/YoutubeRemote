4f952e97f506a891efacf449b91e23c5eca4e942
eochoa5 <edwinochoa57@gmail.com>
2017-06-05 09:19:43 -0700

    remaining: deploy to Heroku, testing, deploy to Google Play

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
 	console.log('a user connected: ' + socket.id);
 
+	io.to(socket.id).emit('key', socket.id);
+
 	socket.on('play', function(data){
 		io.to(data).emit('play', 1);
 		
@@ -20,15 +22,37 @@ io.on('connection', function(socket){
 		
 	});
 
-	
+
 	socket.on('startVideo', function(data){
-		io.to(data.to).emit('startVideo', data.url);
+		io.to(data.to).emit('startVideo', {url:data.url, sender:socket.id});
+
+	});
+
+	socket.on('vidDuration', function(data){
+		io.to(data.to).emit('vidDuration', data.duration);
+
+	});
+
+	socket.on('setVolume', function(data){
+		io.to(data.to).emit('setVolume', data.vol);
 		
 	});
 
-	io.to(socket.id).emit('key', socket.id);
+	socket.on('seekTo', function(data){
+		io.to(data.to).emit('seekTo', data.secs);
+		
+	});
 
+	socket.on('mute', function(data){
+		io.to(data).emit('mute', 1);
+		
+	});
 
+	socket.on('unMute', function(data){
+		io.to(data).emit('unMute', 1);
+		
+	});
+	
 	socket.on('disconnect', function(){
 		console.log('a user disconnected: ' + socket.id);
 	});
